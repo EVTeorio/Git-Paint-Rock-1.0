93bde7704de232f84671f32c1cd8e1f235c18ece
EVTeorio <evanevander303@gmail,com>
2025-05-24 18:29:06 -0500

    Analysis

--- Final Pipline in Order/C_Data_Processing/5_Extracting_Sunlit.R
+++ Final Pipline in Order/C_Data_Processing/5_Extracting_Sunlit.R
@@ -5,11 +5,14 @@ library(dplyr)
 library(tidyr)
 library(beepr)
 beep()
-###################### masking shadow pixels ######################################
+###################### masking shadow pixels ###################################
 data_clean <- read.csv("E:/Thesis_Final_Data/ALLmetrics_clean.csv")
 # Filter rows 
 filtered_data <- data_clean[data_clean$ClAInt >= 2.26424, ]
 
+write.csv(filtered_data,"E:/Thesis_Final_Data/ALLmetrics_clean_sunlit.csv")
+
+################################################################################
 #removing LiDAR Metrics with little information
 Columns_rem <- filtered_data[, !names(filtered_data) %in% c(
   "PAD_40_45_off", "PAD_45_50_off", "PAD_40_45_on", "PAD_45_50_on"
